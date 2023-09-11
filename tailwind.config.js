39abc181fa6cb18ff449f10687c65fb88af50301
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-12 02:08:21 +0300

    navbar + hero done
    adaptive need

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      maxWidth: {
+        container: "1280px",
+      },
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+        "2x1": "1400px",
+      },
+      colors: {
+        mainBg: "#FDF8F6",
+        accentBg: "#582467",
+      }
+    },
+  },
+  plugins: [],
+};
